b9d30e683e77a863ea36c5688ee2b6ad2c035b8e
anton-zm <antonzm34@gmail.com>
2022-05-20 16:26:19 +0300

    add structure, assets

---
+++ src/components/content-wrapper.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Content = styled.div`
+    max-width: 1280px;
+    margin: 0 auto;
+    background-color: transparent;
+    @media (max-width: 1360px){
+        padding: 0 40px;
+    }
+`
