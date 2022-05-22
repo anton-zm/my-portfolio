12a842fd5010dbeb656eff5db5d28e632e980c17
anton-zm <antonzm34@gmail.com>
2022-05-22 13:33:23 +0300

    contacts

--- src/components/contacts.tsx
+++ src/components/contacts.tsx
@@ -1,9 +1,44 @@
 import { observer } from "mobx-react-lite"
+import styled from "styled-components"
+import { useLang } from "../langs"
+import { Section } from "./section"
+import mail from '../assets/images/email.png'
+import tlg from '../assets/images/tlg.png'
+
+const Wrapper = styled.div`
+    width: 60%;
+    margin-top: 2rem;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    @media screen and (max-width: 900px){
+        width: 100%;
+    }
+    @media screen and (max-width: 550px){
+        flex-direction: column;
+    }
+`
+
+const Contact = styled.p<{icon: string}>`
+    color: white;
+    font-size: 1.5rem;
+    margin-bottom: 10px;
+    ::before {
+        content: ${props => `url(${props.icon})`};
+        margin-right: 10px;
+    }
+`
 
 export const Contacts = observer(() => {
+    const lang = useLang()
+    
     return (
-        <>
-           
-        </>
+        <Section cn="flex-column centered-align" id="contacts" title={lang.contacts}>
+            <Wrapper>
+                <Contact className="flex-row centered-align" icon={mail}>antonzm34@gmail.com</Contact>
+                <Contact className="flex-row centered-align" icon={tlg}>@Antonzm</Contact>
+            </Wrapper>
+        </Section>
     )
 })
